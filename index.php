b228a56a1bcea75e340eb77291aa5736a40bbc1d
Mahdi Yazdani <mahdiyazdani@msn.com>
2021-10-08 23:52:10 +0300

    Added WooCommerce version meta-data

--- index.php
+++ index.php
@@ -3,18 +3,20 @@
  * Add to Cart Block.
  *
  * @wordpress-plugin
- * Plugin Name:          Sixa - Add to Cart Block
- * Description:          Display an add-to-cart button for a given product.
- * Version:              1.0.0
- * Requires at least:    5.7
- * Requires PHP:         7.3
- * Author:               sixa AG
- * Author URI:           https://sixa.ch
- * License:              GPL v3 or later
- * License URI:          https://www.gnu.org/licenses/gpl-3.0.html
- * Text Domain:          sixa-block-add-to-cart
+ * Plugin Name:             Sixa - Add to Cart Block
+ * Description:             Display an add-to-cart button for a given product.
+ * Version:                 1.0.0
+ * Requires at least:       5.5
+ * Requires PHP:            7.3
+ * Author:                  sixa AG
+ * Author URI:              https://sixa.ch
+ * License:                 GPL v3 or later
+ * License URI:             https://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain:             sixa-block-add-to-cart
+ * WC requires at least:    4.5
+ * WC tested up to:         5.7
  *
- * @package              Sixa_Blocks
+ * @package                 Sixa_Blocks
  */
 
 defined( 'ABSPATH' ) || exit; // Exit if accessed directly.
