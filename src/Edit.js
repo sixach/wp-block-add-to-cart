7a7402ea8814dfc8fa9cfec42335033a08f4a6ad
Markus <markus.goeckeritz@sixa.ch>
2021-12-06 17:23:24 +0100

    moved checks to separate useEffect

--- src/Edit.js
+++ src/Edit.js
@@ -101,17 +101,26 @@ import Constants from './constants';
 function Edit( { attributes, clientId, backgroundColor, setAttributes, setBackgroundColor, setTextColor, textColor } ) {
 	const styles = {};
 	const { postId, text, textAlign } = attributes;
+	const [ isEditing, toggleIsEditing ] = useToggle( ! Boolean( postId ) );
 	const { isLoading, productsOptions, productsQuery } = useGetProducts( {}, clientId );
 	const { havePosts } = usePreparePosts( [], 1, productsQuery );
 	const nodeList = useGetNodeList( 'sixa-add-to-cart-block/v1/nodes' );
 	const product = useFindPostById( postId, productsQuery );
-	const [ isEditing, toggleIsEditing ] = useToggle( ! Boolean( postId ) || ! Boolean( product ) );
 	const blockProps = useBlockProps( { className: classnames( { [ `has-text-align-${ textAlign }` ]: ! isEditing && textAlign } ) } );
 	const className = blockClassName( blockProps?.className );
 	const { maxStockQuantity, nodes } = useMemo(
 		() => ( { maxStockQuantity: get( product, 'stock_quantity' ), nodes: get( product, 'sixa_add_to_cart_block.nodes' ) } ),
 		[ product ]
 	);
+
+	useEffect( () => {
+		if ( ! product && ! isEditing ) {
+			toggleIsEditing();
+			if ( Boolean( postId ) ) {
+				setAttributes({ postId: undefined });
+			}
+		}
+	}, [ product ] );
 
 	const { gradientClass: backgroundGradientClass, gradientValue: backgroundGradientValue, setGradient: setBackgroundGradient } = __experimentalUseGradient();
 	const { backgroundColorClass, backgroundColorValue, textColorClass, textColorValue } = useMemo(
